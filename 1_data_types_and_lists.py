6b338c4a7a3bb1965bfd815a057e5c3448af7d79
CallousOverlord <kht6@duke.edu>
2015-01-28 15:25:39 -0500

    data types and lists
    class example

---
+++ 1_data_types_and_lists.py
@@ -0,0 +1,7 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+#code goes in GitHub directory, and data goes in main directory
+main_dir = "C:\Users\Kyle\Desktop\data"
+git_dir = "C:\Users\Kyle\Documents\GitHub\PubPol590"
+
